5de7076793eebc1864c0f764f15d42e94a40efcd
Ndunttsr <112713780+Ndunttsr@users.noreply.github.com>
2022-09-10 21:35:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -60,7 +60,7 @@ global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 global.owner = ['+94714568996']
 global.ownertag = '+94714568996' //your tag number
-global.botname = 'FENIX BOT - MDy' //ur bot name
+global.botname = 'FENIX BOT - MD' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
 global.author = "FENIX BOT - MD" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
